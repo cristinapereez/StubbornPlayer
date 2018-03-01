92d7cbc72ccbd5a1f33fcf0849b953ef7b19432e
cristinapereez <cperez58@mail.csuchico.edu>
2018-03-01 11:44:17 -0800

    Record input and increase wins

--- main.cpp
+++ main.cpp
@@ -6,6 +6,23 @@ using namespace std;
 int main()
 {
   SecretDoor game;
-    
+  int response;
+  int wins = 0;
+  
+  cout<<"How many times do you want to play the game?"<<endl;
+  cin>>response;
+  
+for( int i = 0; i < response; i++)
+  {
+  game.newGame();
+  game.guessDoorC();
+  game.guessDoorC();
+  if(game.isWinner() == true)
+  {
+    wins++;
+  }
+  }
+ cout<<"You won "<<wins<<endl;
+  
   return 0;
 }
